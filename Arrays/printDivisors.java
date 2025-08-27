8f5502cadddf2ff2b173526cd0fca2b58bcb4573
HarshGupta <harshgupta0907@gmail.com>
2025-08-27 12:07:45 +0530

    printDivisors

---
+++ Arrays/printDivisors.java
@@ -0,0 +1,20 @@
+package Arrays;
+import java.util.*;
+
+public class printDivisors {
+    public static void main(String[] args) {
+        int n = 10;
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+                if (i != n / i) { 
+                    list.add(n / i);
+                }
+            }
+        }
+        
+        System.out.println(list);
+    }
+}
