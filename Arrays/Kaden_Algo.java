2b5abbc718b6f76f748d26e8f5a730a27522e616
HarshGupta <harshgupta0907@gmail.com>
2025-08-31 17:44:54 +0530

    Kaden's Algorithm

---
+++ Arrays/Kaden_Algo.java
@@ -0,0 +1,10 @@
+public class Kaden_Algo {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0], maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
