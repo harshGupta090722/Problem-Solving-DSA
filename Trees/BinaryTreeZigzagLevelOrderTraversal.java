5f06dfb3922b31dce52512978d94bdef439e71f1
Harsh Gupta <harshgupta0907@gmail.com>
2025-12-17 16:40:40 +0530

    Trees

---
+++ Trees/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    
+    static class  TreeNode{
+        int val;
+        TreeNode left,right;
+        
+        TreeNode(int val){
+            this.val=val;
+            this.right=null;
+            this.left=null;
+        }
+    }
+    
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list=new ArrayList<>();
+        
+    }
+    public static void main(String args[]){
+
+        TreeNode root=new TreeNode(3);
+        root.left=new TreeNode(9);
+        root.right=new TreeNode(20);
+        root.right.left=new TreeNode(15);
+        root.right.right=new TreeNode(7);
+
+        System.out.println(zigzagLevelOrder(root));
+    }
+}
---
+++ Trees/MaximumPathSum.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class MaximumPathSum {
+
+    static class  TreeNode{
+        int val;
+        TreeNode left,right;
+        
+        TreeNode(int val){
+            this.val=val;
+            this.right=null;
+            this.left=null;
+        }
+    }
+
+    public static int maxPathSum(TreeNode root) {
+        int maxValue[]=new int[1];
+        maxValue[0]=Integer.MIN_VALUE;
+        maxPathDown(root,maxValue);
+        return maxValue[0];
+    }
+    
+    private static int maxPathDown(TreeNode node,int maxValue[]){
+        if(node==null)
+            return 0;
+        
+        int left=Math.max(0,maxPathDown(node.left, maxValue));
+        int right=Math.max(0,maxPathDown(node.right, maxValue));
+        
+        maxValue[0]=Math.max(maxValue[0],left+right+node.val);
+        return Math.max(left,right)+node.val;
+    }
+    
+    public static void main(String args[]){
+        
+        TreeNode root=new TreeNode(-10);
+        root.left=new TreeNode(9);
+        root.right=new TreeNode(20);
+        root.right.left=new TreeNode(15);
+        root.right.right=new TreeNode(7);
+
+        System.out.println(maxPathSum(root));
+    }
+}
